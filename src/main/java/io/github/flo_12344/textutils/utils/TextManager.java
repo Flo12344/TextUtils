65f24e8a24ac878ae1ccb2d04e0a2996a0cd994b
Flouttre <florent.daerden38@gmail.com>
2026-02-03 18:55:15 +0100

    Added empty line cleanup

--- src/main/java/io/github/flo_12344/textutils/utils/TextManager.java
+++ src/main/java/io/github/flo_12344/textutils/utils/TextManager.java
@@ -22,53 +22,53 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class TextManager {
-    public static void SpawnText3dEntity(Vector3d pos, Vector3f rot, @Nonnull World world, String text, String _label, String font, float size) {
-        world.execute(() -> {
-            Store<EntityStore> store = world.getEntityStore().getStore();
-            Holder<EntityStore> holder = EntityStore.REGISTRY.newHolder();
+public class TextManager{
+  public static void SpawnText3dEntity(Vector3d pos, Vector3f rot, @Nonnull World world, String text, String _label, String font, float size){
+    world.execute(() -> {
+      Store<EntityStore> store = world.getEntityStore().getStore();
+      Holder<EntityStore> holder = EntityStore.REGISTRY.newHolder();
 
-            TransformComponent transform = new TransformComponent(pos, rot);
+      TransformComponent transform = new TransformComponent(pos, rot);
 
-            holder.addComponent(TransformComponent.getComponentType(), transform);
-            holder.addComponent(NetworkId.getComponentType(), new NetworkId(store.getExternalData().takeNextNetworkId()));
-            var uuid = holder.ensureAndGetComponent(UUIDComponent.getComponentType());
-            holder.addComponent(Intangible.getComponentType(), Intangible.INSTANCE);
-            holder.addComponent(TextUtils3DTextComponent.getComponentType(),
-                    new TextUtils3DTextComponent(font, text, _label, size));
-            store.addEntity(holder, AddReason.SPAWN);
-            TextUtilsHologramRegistry.get().onEntityAdded(_label, uuid.getUuid());
-        });
-    }
+      holder.addComponent(TransformComponent.getComponentType(), transform);
+      holder.addComponent(NetworkId.getComponentType(), new NetworkId(store.getExternalData().takeNextNetworkId()));
+      var uuid = holder.ensureAndGetComponent(UUIDComponent.getComponentType());
+      holder.addComponent(Intangible.getComponentType(), Intangible.INSTANCE);
+      holder.addComponent(TextUtils3DTextComponent.getComponentType(),
+              new TextUtils3DTextComponent(font, text, _label, size));
+      store.addEntity(holder, AddReason.SPAWN);
+      TextUtilsHologramRegistry.get().onEntityAdded(_label, uuid.getUuid());
+    });
+  }
 
-    public static void RemoveText3dEntity(String id, World world, Store<EntityStore> store) {
-        var text_entity = world.getEntityRef(TextUtilsHologramRegistry.get().getUUID(id));
-        store.addComponent(text_entity, Text3dDeleterComponent.getComponentType());
-    }
+  public static void RemoveText3dEntity(String id, World world, Store<EntityStore> store){
+    var text_entity = world.getEntityRef(TextUtilsHologramRegistry.get().getUUID(id));
+    store.addComponent(text_entity, Text3dDeleterComponent.getComponentType());
+  }
 
-    public static void ResizeText3dEntity(String id, World world, Store<EntityStore> store, float size) {
-        var text_entity = world.getEntityRef(TextUtilsHologramRegistry.get().getUUID(id));
-        var textUtilsEntity = store.getComponent(text_entity, TextUtils3DTextComponent.getComponentType());
-        textUtilsEntity.setSize(size);
-    }
+  public static void ResizeText3dEntity(String id, World world, Store<EntityStore> store, float size){
+    var text_entity = world.getEntityRef(TextUtilsHologramRegistry.get().getUUID(id));
+    var textUtilsEntity = store.getComponent(text_entity, TextUtils3DTextComponent.getComponentType());
+    textUtilsEntity.setSize(size);
+  }
 
-    public static void MoveText3dEntity(String id, World world, Store<EntityStore> store, Vector3d pos) {
-        TransformText3dEntity(id, world, store, new Vector3f(), pos);
-    }
+  public static void MoveText3dEntity(String id, World world, Store<EntityStore> store, Vector3d pos){
+    TransformText3dEntity(id, world, store, new Vector3f(), pos);
+  }
 
-    public static void RotateText3dEntity(String id, World world, Store<EntityStore> store, Vector3f rot) {
-        TransformText3dEntity(id, world, store, new Vector3f((float) Math.toRadians(rot.x), (float) Math.toRadians(rot.y), (float) Math.toRadians(rot.z)), new Vector3d());
-    }
+  public static void RotateText3dEntity(String id, World world, Store<EntityStore> store, Vector3f rot){
+    TransformText3dEntity(id, world, store, new Vector3f((float) Math.toRadians(rot.x), (float) Math.toRadians(rot.y), (float) Math.toRadians(rot.z)), new Vector3d());
+  }
 
-    public static void TransformText3dEntity(String id, World world, Store<EntityStore> store, Vector3f rot, Vector3d pos) {
-        var text_entity = world.getEntityRef(TextUtilsHologramRegistry.get().getUUID(id));
-        var textUtilsEntity = store.getComponent(text_entity, TextUtils3DTextComponent.getComponentType());
-        TransformComponent transform = store.getComponent(text_entity, TransformComponent.getComponentType());
-        if (transform == null)
-            return;
-        transform.getPosition().add(pos);
-        transform.getRotation().add(rot);
-        textUtilsEntity.setMoved(true);
+  public static void TransformText3dEntity(String id, World world, Store<EntityStore> store, Vector3f rot, Vector3d pos){
+    var text_entity = world.getEntityRef(TextUtilsHologramRegistry.get().getUUID(id));
+    var textUtilsEntity = store.getComponent(text_entity, TextUtils3DTextComponent.getComponentType());
+    TransformComponent transform = store.getComponent(text_entity, TransformComponent.getComponentType());
+    if(transform == null)
+      return;
+    transform.getPosition().add(pos);
+    transform.getRotation().add(rot);
+    textUtilsEntity.setMoved(true);
 //        int text_pos = 0;
 //        float width = (float) FontManager.INSTANCE.getFontSettings(textUtilsEntity.getFont_name()).max_width / 64;
 //        var arr = textUtilsEntity.getText_entities();
@@ -82,39 +82,44 @@ public class TextManager {
 //            t.setRotation(transform.getRotation());
 //            text_pos++;
 //        }
+  }
+
+  public static void SetText3dVisibility(String id, World world, Store<EntityStore> store, Boolean visible){
+    var text_entity = world.getEntityRef(TextUtilsHologramRegistry.get().getUUID(id));
+    var txt = store.getComponent(text_entity, TextUtils3DTextComponent.getComponentType());
+    txt.setVisible(visible);
+  }
+
+  public static void EditText3dContent(String id, World world, Store<EntityStore> store, String content){
+    var text_entity = world.getEntityRef(TextUtilsHologramRegistry.get().getUUID(id));
+    var txt = world.getEntityStore().getStore().getComponent(text_entity, TextUtils3DTextComponent.getComponentType());
+    txt.setText(content);
+  }
+
+  public static void EditText3dLine(String id, World world, Store<EntityStore> store, String content, int line){
+    var text_entity = world.getEntityRef(TextUtilsHologramRegistry.get().getUUID(id));
+    var txt = world.getEntityStore().getStore().getComponent(text_entity, TextUtils3DTextComponent.getComponentType());
+    String current = txt.getText();
+    List<String> lines = new ArrayList<>(Arrays.asList(current.split("\\\\n")));
+    Universe.get().sendMessage(Message.raw(current));
+    Universe.get().sendMessage(Message.raw(String.valueOf(lines.size())));
+    while(lines.size() <= line){
+      lines.add("");
+    }
+    Universe.get().sendMessage(Message.raw(String.valueOf(lines.size())));
+    lines.set(line, content);
+
+    while(Objects.equals(lines.getLast(), "\\n") || lines.getLast().isEmpty()){
+      lines.removeLast();
     }
 
-    public static void SetText3dVisibility(String id, World world, Store<EntityStore> store, Boolean visible) {
-        var text_entity = world.getEntityRef(TextUtilsHologramRegistry.get().getUUID(id));
-        var txt = store.getComponent(text_entity, TextUtils3DTextComponent.getComponentType());
-        txt.setVisible(visible);
-    }
+    current = String.join("\\n", lines);
+    txt.setText(current);
+  }
 
-    public static void EditText3dContent(String id, World world, Store<EntityStore> store, String content) {
-        var text_entity = world.getEntityRef(TextUtilsHologramRegistry.get().getUUID(id));
-        var txt = world.getEntityStore().getStore().getComponent(text_entity, TextUtils3DTextComponent.getComponentType());
-        txt.setText(content);
-    }
-
-    public static void EditText3dLine(String id, World world, Store<EntityStore> store, String content, int line) {
-        var text_entity = world.getEntityRef(TextUtilsHologramRegistry.get().getUUID(id));
-        var txt = world.getEntityStore().getStore().getComponent(text_entity, TextUtils3DTextComponent.getComponentType());
-        String current = txt.getText();
-        List<String> lines = new ArrayList<>(Arrays.asList(current.split("\\\\n")));
-        Universe.get().sendMessage(Message.raw(current));
-        Universe.get().sendMessage(Message.raw(String.valueOf(lines.size())));
-        while (lines.size() <= line) {
-            lines.add("");
-        }
-        Universe.get().sendMessage(Message.raw(String.valueOf(lines.size())));
-        lines.set(line, content);
-        current = String.join("\\n", lines);
-        txt.setText(current);
-    }
-
-    public static void ChangeText3dFont(String id, World world, Store<EntityStore> store, String font_id) {
-        var text_entity = world.getEntityRef(TextUtilsHologramRegistry.get().getUUID(id));
-        var txt = world.getEntityStore().getStore().getComponent(text_entity, TextUtils3DTextComponent.getComponentType());
-        txt.setFont_name(font_id);
-    }
+  public static void ChangeText3dFont(String id, World world, Store<EntityStore> store, String font_id){
+    var text_entity = world.getEntityRef(TextUtilsHologramRegistry.get().getUUID(id));
+    var txt = world.getEntityStore().getStore().getComponent(text_entity, TextUtils3DTextComponent.getComponentType());
+    txt.setFont_name(font_id);
+  }
 }
